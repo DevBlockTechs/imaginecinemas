3c469783c716a78515ce45580fd72ee054507f8b
cea@devblock <ceo@devblocktechnologies.org>
2023-07-25 18:29:50 +0100

    m

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header =()=> {
+  return (
+    <header>
+        <div className="logo">
+            IMAGINE CINEMAS
+        </div>
+        <div className="menu">
+            <div className="jsline top"></div>
+            <div className="jsline middle"></div>
+            <div className="jsline bottom"></div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
