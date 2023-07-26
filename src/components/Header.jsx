b7dc4c83c5cd78cc12c746459b44b6a42a925394
cea@devblock <ceo@devblocktechnologies.org>
2023-07-26 10:38:33 +0100

    m

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ const Header =()=> {
 
     useEffect(()=>{
         window.addEventListener('scroll', ()=>{
-            if(window.scrollY > 150){
+            if(window.scrollY > 20){
                 document.getElementById('logo').style.opacity = 0
             }
             
-            if(window.scrollY < 150){
+            if(window.scrollY < 20){
                 document.getElementById('logo').style.opacity = 1
             }
         })
@@ -20,10 +20,16 @@ const Header =()=> {
         <div id="logo">
             IMAGINE CINEMAS
         </div>
-        <div className="menu">
-            <div className="jsline top"></div>
+            <div className="menu">
+                <div style={{
+                    display : 'flex',
+                    flexDirection : 'column',
+                    gap : 4,
+                }}>
+                <div className="jsline top"></div>
             <div className="jsline middle"></div>
             <div className="jsline bottom"></div>
+                </div>
         </div>
     </header>
   )
